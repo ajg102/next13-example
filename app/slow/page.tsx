e9e512db9533c7d6efb038c7e24a77475c663f90
ajg102 <alexglick102@gmail.com>
2023-07-31 17:42:57 -0400

    run it back

--- app/slow/page.tsx
+++ app/slow/page.tsx
@@ -1,39 +1,47 @@
+import { isFulfilled } from "@/lib/helpers";
 import Image from "next/image";
 import { Pokemon } from "./types";
 
 const getPokemon = async (id: string | number) => {
-  const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {
-    cache: "no-cache",
-    next: {
-      // just noting this is a thing?
-    },
-  });
-  const parsedRes = await data.json();
-  return parsedRes as Pokemon;
+  try {
+    const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {
+      cache: "no-cache",
+      next: {
+        // just noting this is a thing?
+      },
+    });
+    const parsedRes = await data.json();
+    return parsedRes as Pokemon;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
 };
 
 export default async function Slow() {
-  const promises = Array.from({ length: 1008 }).map((_, index) =>
+  const promises = Array.from({ length: 151 }).map((_, index) =>
     getPokemon(index + 1)
   );
-  const pokemon = await Promise.all(promises);
+  const data = await Promise.allSettled(promises);
+  const pokemon = data.filter(isFulfilled);
+
   return (
     <div className="w-full grid grid-cols-3 gap-6">
       <h1 className="col-span-full">Really Slow Loading</h1>
-      {pokemon.map((poke) => (
+      {pokemon.map(({ value }) => (
         <div
-          key={poke.id}
+          key={value.id}
           className="col-span-1 border-white border text-white bg-black"
         >
           <Image
             width={50}
             height={50}
-            src={poke.sprites.front_default}
-            alt={poke.name}
+            src={value.sprites.front_default}
+            alt={value.name}
           />
-          <p>{poke.name}</p>
+          <p>{value.name}</p>
           <p>
-            {poke.types.map((type) => (
+            {value.types.map((type) => (
               <div
                 key={type.type.name}
                 className="rounded-full px-3 py-2 bg-yellow-600 text-white"
